7f59fb30678a32fd2efbbdde96cc33c1349643d8
zeng007 <scarlettzing@hotmail.com>
2019-02-13 11:20:29 -0500

    Add files via upload

---
+++ js/scents.js
@@ -0,0 +1,235 @@
+var prompts = [
+{
+	prompt: 'I would like to spend time in nature than city on weekends',
+	weight: -1,
+	class: 'group0'
+},
+{
+	prompt: 'I prefet cool color than warm color ',
+	weight: -1,
+	class: 'group1'
+},
+{
+	prompt: 'I prefer to spend time alone than go to a huge party',
+	weight: -1,
+	class: 'group2'
+},
+{
+	prompt: 'I would wear confertable cloth than fansy cloth',
+	weight: -1,
+	class: 'group3'
+},
+{
+	prompt: 'I like unisex scents',
+	weight: -1,
+	class: 'group4'
+},
+{
+	prompt: 'I prefer strong scents than floral scents ',
+	weight: -1,
+	class: 'group5'
+},
+{
+	prompt: 'I am usually highly motivated and energetic',
+	weight: 1,
+	class: 'group6'
+},
+{
+	prompt: 'I prefer dramatic makeup than natural makeup',
+	weight: 1,
+	class: 'group7'
+},
+{
+	prompt: 'I only like feminine scents ',
+	weight: 1,
+	class: 'group8'
+},
+{
+	prompt: 'I am a moody and sensitive person',
+	weight: 1,
+	class: 'group9'
+},
+
+
+]
+
+// This array stores all of the possible values and the weight associated with the value.
+// The stronger agreeance/disagreeance, the higher the weight on the user's answer to the prompt.
+var prompt_values = [
+{
+	value: 'Strongly Agree',
+	class: 'btn-default btn-strongly-agree',
+	weight: 5
+},
+{
+	value: 'Agree',
+	class: 'btn-default btn-agree',
+	weight: 3,
+},
+{
+	value: 'Neutral',
+	class: 'btn-default',
+	weight: 0
+},
+{
+	value: 'Disagree',
+	class: 'btn-default btn-disagree',
+	weight: -3
+},
+{
+	value: 'Strongly Disagree',
+	class: 'btn-default btn-strongly-disagree',
+	weight: -5
+}
+]
+
+// For each prompt, create a list item to be inserted in the list group
+function createPromptItems() {
+
+	for (var i = 0; i < prompts.length; i++) {
+		var prompt_li = document.createElement('li');
+		var prompt_p = document.createElement('p');
+		var prompt_text = document.createTextNode(prompts[i].prompt);
+
+		prompt_li.setAttribute('class', 'list-group-item prompt');
+		prompt_p.appendChild(prompt_text);
+		prompt_li.appendChild(prompt_p);
+
+		document.getElementById('quiz').appendChild(prompt_li);
+	}
+}
+
+// For each possible value, create a button for each to be inserted into each li of the quiz
+// function createValueButtons() {
+
+// 	for (var li_index = 0; li_index < prompts.length; li_index++) {
+// 		for (var i = 0; i < prompt_values.length; i++) {
+// 			var val_button = document.createElement('button');
+// 			var val_text = document.createTextNode(prompt_values[i].value);
+
+// 			val_button.setAttribute('class', 'value-btn btn ' + prompt_values[i].class);
+// 			val_button.appendChild(val_text);
+
+// 			document.getElementsByClassName('prompt')[li_index].appendChild(val_button);
+// 		}
+// 	}
+// }
+function createValueButtons() {
+	for (var li_index = 0; li_index < prompts.length; li_index++) {
+		var group = document.createElement('div');
+		group.className = 'btn-group btn-group-justified';
+
+		for (var i = 0; i < prompt_values.length; i++) {
+			var btn_group = document.createElement('div');
+			btn_group.className = 'btn-group';
+
+			var button = document.createElement('button');
+			var button_text = document.createTextNode(prompt_values[i].value);
+			button.className = 'group' + li_index + ' value-btn btn ' + prompt_values[i].class;
+			button.appendChild(button_text);
+
+			btn_group.appendChild(button);
+			group.appendChild(btn_group);
+
+			document.getElementsByClassName('prompt')[li_index].appendChild(group);
+		}
+	}
+}
+
+createPromptItems();
+createValueButtons();
+
+// Keep a running total of the values they have selected. If the total is negative, the user is introverted. If positive, user is extroverted.
+// Calculation will sum all of the answers to the prompts using weight of the value * the weight of the prompt.
+var total = 0;
+
+// Get the weight associated to group number
+function findPromptWeight(prompts, group) {
+	var weight = 0;
+
+	for (var i = 0; i < prompts.length; i++) {
+		if (prompts[i].class === group) {
+			weight = prompts[i].weight;
+		}
+	}
+
+	return weight;
+}
+
+// Get the weight associated to the value
+function findValueWeight(values, value) {
+	var weight = 0;
+
+	for (var i = 0; i < values.length; i++) {
+		if (values[i].value === value) {
+			weight = values[i].weight;
+		}
+	}
+
+	return weight;
+}
+
+// When user clicks a value to agree/disagree with the prompt, display to the user what they selected
+$('.value-btn').mousedown(function () {
+	var classList = $(this).attr('class');
+	// console.log(classList);
+	var classArr = classList.split(" ");
+	// console.log(classArr);
+	var this_group = classArr[0];
+	// console.log(this_group);
+
+	// If button is already selected, de-select it when clicked and subtract any previously added values to the total
+	// Otherwise, de-select any selected buttons in group and select the one just clicked
+	// And subtract deselected weighted value and add the newly selected weighted value to the total
+	if($(this).hasClass('active')) {
+		$(this).removeClass('active');
+		total -= (findPromptWeight(prompts, this_group) * findValueWeight(prompt_values, $(this).text()));
+	} else {
+		// $('[class='thisgroup).prop('checked', false);
+		total -= (findPromptWeight(prompts, this_group) * findValueWeight(prompt_values, $('.'+this_group+'.active').text()));
+		// console.log($('.'+this_group+'.active').text());
+		$('.'+this_group).removeClass('active');
+
+		// console.log('group' + findValueWeight(prompt_values, $('.'+this_group).text()));
+		// $(this).prop('checked', true);
+		$(this).addClass('active');
+		total += (findPromptWeight(prompts, this_group) * findValueWeight(prompt_values, $(this).text()));
+	}
+
+	console.log(total);
+})
+
+
+
+$('#submit-btn').click(function () {
+	// After clicking submit, add up the totals from answers
+	// For each group, find the value that is active
+	$('.results').removeClass('hide');
+	$('.results').addClass('show');
+
+	if(total < 0) {
+		// document.getElementById('intro-bar').style.width = ((total / 60) * 100) + '%';
+		// console.log(document.getElementById('intro-bar').style.width);
+		// document.getElementById('intro-bar').innerHTML= ((total / 60) * 100) + '%';
+		document.getElementById('results').innerHTML = '<b>Your perfect match is oriental woody!</b><br><br>';
+	} else if(total > 0) {
+		document.getElementById('results').innerHTML = '<b>Your perfect match is soft floral!</b><br><br>';
+	} else {
+		document.getElementById('results').innerHTML = '<b>Your perfect match is fresh citrus!</b><br><br>'
+	}
+
+	// Hide the quiz after they submit their results
+	$('#quiz').addClass('hide');
+	$('#submit-btn').addClass('hide');
+	$('#retake-btn').removeClass('hide');
+})
+
+// Refresh the screen to show a new quiz if they click the retake quiz button
+$('#retake-btn').click(function () {
+	$('#quiz').removeClass('hide');
+	$('#submit-btn').removeClass('hide');
+	$('#retake-btn').addClass('hide');
+
+	$('.results').addClass('hide');
+	$('.results').removeClass('show');
+})
